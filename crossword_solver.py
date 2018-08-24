6d9d63e5b7e1f56832604855f1497ef7be9fa38b
harborned <d.harborne@outlook.com>
2018-08-24 23:43:22 +0100

    initial commit

---
+++ crossword_solver.py
@@ -0,0 +1,20 @@
+### algorithm approach
+
+#function that takes in incomplete word and returns list of possible words (can take in possible letters)
+
+#function that takes in a word and a vector space and a clue and finds the closest semantic match
+
+#function that takes a crossword, looks for clue with highest confidence and attempts to solve it
+
+
+
+### googling approach
+
+
+
+
+### exhaustive search approach
+
+
+
+### trained network with knowledge base approach
