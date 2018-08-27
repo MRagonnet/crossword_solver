c7a2e8244caccdace96bd565c829452399dd777a
harborned <d.harborne@outlook.com>
2018-08-27 23:38:35 +0100

    guadian scrape complete

--- guardian_scrape.py
+++ guardian_scrape.py
@@ -58,6 +58,49 @@ def GetCrosswordNumberGridIndexs(crossword_page_tree):
 
     return grid_indexs
 
+def GetClueTextandWordLength(crossword_page_tree):
+    down_clue_nums = crossword_page_tree.xpath('//div[@class="crossword__clues--down"]/ol/li/@value')
+    down_clue_text_and_length = crossword_page_tree.xpath('//div[@class="crossword__clues--down"]/ol/li/div[@class="crossword__clue__text"]/text()')
+    
+    down_clue_text = [ "".join(text.split("(")[:-1]).strip() for text in down_clue_text_and_length]
+    down_clue_length = [ "("+text.split("(")[-1] for text in down_clue_text_and_length]
+
+    down_clues = zip(down_clue_nums,down_clue_length,down_clue_text)
+    
+    across_clue_nums = crossword_page_tree.xpath('//div[@class="crossword__clues--across"]/ol/li/@value')
+    across_clue_text_and_length = crossword_page_tree.xpath('//div[@class="crossword__clues--across"]/ol/li/div[@class="crossword__clue__text"]/text()')
+    
+    across_clue_text = [ "".join(text.split("(")[:-1]).strip() for text in across_clue_text_and_length]
+    across_clue_length = [ "("+text.split("(")[-1] for text in across_clue_text_and_length]
+
+    across_clues = zip(down_clue_nums,down_clue_length,down_clue_text)
+    
+    return across_clues, down_clues
+
+def OutputClueDetails(crossword_id, grid_indexs, across_clues, down_clues):
+    clues = []
+    for clue in across_clues:
+        clue_i = int(clue[0])
+        clues.append( (grid_indexs[clue_i][0],grid_indexs[clue_i][1],clue[1],"across",clue[2]) )
+
+    for clue in down_clues:
+        clue_i = int(clue[0])
+        clues.append( (grid_indexs[clue_i][0],grid_indexs[clue_i][1],clue[1],"down",clue[2]) )
+
+    crossword_clue_dir = "crossword_clues"
+
+    output_path = os.path.join(crossword_clue_dir,crossword_id+".txt")
+
+    clue_output_text = ""
+
+    for clue in clues:
+        clue_output_text += str(clue)[1:-1] + "\n"
+
+    clue_output_text = clue_output_text[:-1]
+
+    with open(output_path,"w") as f:
+        f.write(clue_output_text)
+    
 
 if __name__ == '__main__':
 
@@ -67,5 +110,14 @@ if __name__ == '__main__':
     grid_indexs = GetCrosswordNumberGridIndexs(crossword_page_tree)
 
     print(grid_indexs)
+    print("")
 
+    across_clues, down_clues = GetClueTextandWordLength(crossword_page_tree)
 
+    print(across_clues)
+    print("")
+
+    print(down_clues)
+    print("")
+
+    OutputClueDetails(crossword_id, grid_indexs, across_clues, down_clues)
